21a511b25b1e57fff15b98ef1559f7626be1d85d
einarh13 <einarh13@ru.is>
2015-09-17 15:09:00 +0000

    added leapyear logic

--- Web Services/Week05/CoursesAPI.Services/Utilities/DateTimeUtils.cs
+++ Web Services/Week05/CoursesAPI.Services/Utilities/DateTimeUtils.cs
@@ -5,6 +5,10 @@
 		public static bool IsLeapYear(int year)
 		{
 			// TODO: implement!
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+
+                return true;
+            }
 			return false;
 		}
 	}
