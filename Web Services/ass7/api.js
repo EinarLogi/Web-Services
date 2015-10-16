8c290383ec372ff301570d633115240c2bd0e4ad
vignirk <vignirk@gmail.com>
2015-10-16 17:32:10 +0000

    GET og POST a company virkar og eg aetla i dosi

--- Web Services/ass7/api.js
+++ Web Services/ass7/api.js
@@ -12,30 +12,41 @@ const models = require('./models');
 
 const api = express();
 
-//const port = 8080;
-
 api.use(bodyParser.json());
+
+api.get('/company', (req, res) => {
+	models.Company.find({}, (err, docs) => {
+		if(err) {
+			res.status(500).send(err);	// ATH
+			return;
+		}
+		else {
+			res.status(200);
+			res.send(docs);
+		}
+	});
+});
 
 /**
  * Adds a new company to the system.
  */
-api.post('/company', (req,res) => {
+api.post('/company', (req, res) => {
 	const data = req.body;
 	console.log(data);
 
 	const newCompany = new models.Company(data);
-	newCompany.save(function(err, doc) {
+	newCompany.save(function(err, docs) {
 		if(err) {
 			res.status(500).send(err);	// ATH
 			return;
 		}
 		else {
 			res.status(201);
-			res.send(doc);
+			res.send(docs);
 			return;
 		}
 	})
-	
+
 });
 
 
