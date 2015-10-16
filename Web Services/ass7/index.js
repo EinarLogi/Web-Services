c54e953e8b6906a5955594ba420d2768a887422c
vignirk <vignirk@gmail.com>
2015-10-16 16:25:36 +0000

    mongoose connection

--- Web Services/ass7/index.js
+++ Web Services/ass7/index.js
@@ -2,12 +2,21 @@
 
 const express = require('express');
 const api = require('./api');
-const port = 8080;
 
+const mongoose = require('mongoose');
+const port = 8080;
 const app = express();
 
 app.use('/api', api);
 
-app.listen(port, function() {
-	console.log('Server starting on port', port);
+/* Connect to MongoDB */
+const url = 'localhost/punchcardApp';
+mongoose.connect(url);
+mongoose.connection.once('open', function() {
+	console.log('mongoose is connected');
+	app.listen(port, function() {
+		console.log('Server starting on port', port);
+	});
 });
+
+
